90920a50da01ccd2ceee4d4969552bb9daaee15f
Maxime Lachance <maximelachance2002@gmail.com>
2021-01-18 09:35:48 -0500

    d8 p7 index

--- HTML_BS4/assets/js/quote.js
+++ HTML_BS4/assets/js/quote.js
@@ -10,7 +10,7 @@ $("#number_of_hours_of_activity_in_the_building").hide();
 $("#RecoElev").hide();
 $("#InstaCost").hide();
 $("#PricePerElevator").hide();
-$("#TotalElev").hide();
+$("#TotalCostforElev").hide();
 $("#PriceTotal").hide();
 
 $("#Building_option").on('change', function (){
@@ -32,7 +32,7 @@ $("#Building_option").on('change', function (){
         $("#RecoElev").hide();
         $("#InstaCost").hide();
         $("#PricePerElevator").hide();
-        $("#TotalElev").hide();
+        $("#TotalCostforElev").hide();
         $("#PriceTotal").hide();
     };
     
@@ -49,7 +49,7 @@ $("#Building_option").on('change', function (){
         $("#RecoElev").show();
         $("#InstaCost").show();
         $("#PricePerElevator").show();
-        $("#TotalElev").show();
+        $("#TotalCostforElev").show();
         $("#PriceTotal").show();
     };
 
@@ -66,7 +66,7 @@ $("#Building_option").on('change', function (){
         $("#RecoElev").show();
         $("#InstaCost").show();
         $("#PricePerElevator").show();
-        $("#TotalElev").show();
+        $("#TotalCostforElev").show();
         $("#PriceTotal").show();
     };
 
@@ -83,7 +83,7 @@ $("#Building_option").on('change', function (){
         $("#RecoElev").show();
         $("#InstaCost").show();
         $("#PricePerElevator").show();
-        $("#TotalElev").show();
+        $("#TotalCostforElev").show();
         $("#PriceTotal").show();
     };
 
@@ -100,7 +100,7 @@ $("#Building_option").on('change', function (){
         $("#RecoElev").show();
         $("#InstaCost").show();
         $("#PricePerElevator").show();
-        $("#TotalElev").show();
+        $("#TotalCostforElev").show();
         $("#PriceTotal").show();
     };
 
@@ -116,7 +116,85 @@ $("#Building_option").on('change', function (){
     var NumberOfTenantCompanies = document.getElementById("number_of_tenant_companies_input");
     var NumberOfOccupentsPerFloors = document.getElementById("number_of_occupents_per_floors_input");
     var NumberOfHoursOfActivityInTheBuilding = document.getElementById("number_of_hours_of_activity_in_the_building_input");
+    var RecommendedElevator = document.getElementById("ElevatorRec_input");
+    var InstaCost = document.getElementById("InstaCost_input");
+    var PricePerElevator = document.getElementById("PricePerElevator_input");
+    var TotalCostforElev = document.getElementById("TotalCostforElev_input");
+    var PriceTotal = document.getElementById("PriceTotal_input");
+
+    function commercial () {
+        RecommendedElevator.value = NumberOfElevatorCages.value
+    };
+
+    function residential () {
+      var AverageOfApartment = NumberOfAppartment.value / NumberOfFloors.value;
+      console.log(AverageOfApartment + " taf")
+      var elevCage = Math.ceil(AverageOfApartment / 6);
+      console.log(elevCage + " elevCage")
+      var numer_colon = Math.ceil(NumberOfFloors.value / 20);
+      console.log(numer_colon)
+      var ElevatorPerColon =  numer_colon * elevCage;
+      console.log(ElevatorPerColon)
+      RecommendedElevator.value = ElevatorPerColon
+    };
+
+    function Corporate_Hybrid () {
+        var TotalFloors = parseInt(NumberOfFloors.value) + parseInt(NumberOfBasement.value)
+        console.log(TotalFloors)
+        var AllOccupents = NumberOfOccupentsPerFloors.value * TotalFloors
+        console.log(AllOccupents)
+        var TotalElevator = Math.ceil(AllOccupents / 1000)
+        console.log(TotalElevator)
+        var NumColon = TotalElevator / 20
+        console.log(NumColon.value)
+        var ElevatorPerColon = TotalElevator / NumColon
+        console.log(ElevatorPerColon.value)
+        var TotalNbOfelevator = ElevatorPerColon * NumColon
+        console.log(TotalNbOfelevator)
+        RecommendedElevator.value = TotalNbOfelevator
+    };
 
 
+    $("input[type='radio']").on('click', function(){
+        var type = $("input[name='services']:checked").val();
+        if(type == "7565"){
+            PricePerElevator.value = type;
+            TotalCostforElev.value = PricePerElevator.value * RecommendedElevator.value
+        }
 
+        if(type == "12345"){
+            PricePerElevator.value = type;
+        }
+
+        if(type == "15400"){
+            PricePerElevator.value = type;
+        }
+
+
+    });
+
+    $("input").on('keyup click',function () {
+        var TypeValue = $("#Building_option option:selected").val();
+       
+       if(TypeValue=="residential"){
+        residential();
+        
+       }
+    
+       if(TypeValue=="commercial"){
+        commercial();
+        
+              
+       }
+       if(TypeValue=="corporate"){
+        Corporate_Hybrid();
+             
+       }
+    
+       if(TypeValue=="hybrid"){
+        Corporate_Hybrid();
+       
+       }
+      
+    });
     
